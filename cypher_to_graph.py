cec0d5052afa18bbdf17520f113fd212409fef4d
constantin.andreescu <constantin.andreescu@emag.ro>
2020-07-19 18:51:44 +0300

    Finished parsing basic cypher statements and building graph

--- cypher_to_graph.py
+++ cypher_to_graph.py
@@ -1,5 +1,5 @@
 import re
-from graph import Graph
+from graph import Graph, Node, Edge
 
 
 def cypher_to_graph(input_file):
@@ -12,36 +12,77 @@ def cypher_to_graph(input_file):
             print(f"Line: {line}")
             if 'SET' in line:
                 args = re.match(re.compile(
-                    '^SET ([0-9a-zA-Z]+).([a-zA-Z]+) = \'?([0-9a-zA-Z]+)\'?$'
+                    '^SET ([0-9a-zA-Z]+).([a-zA-Z]+) = \'?([0-9a-zA-Z\s\-_]+)\'?$'
                 ), line).groups()
                 if len(args) != 3:
-                    print("Nasol")
-                    exit(1)
+                    print(f"Unable to parse cypher SET statement: {line}"
+                          "The resulting table will not contain the effect of this statement, "
+                          f"so you should probably add it by hand in the table")
+                    continue
+                set_property(graph=graph, node_name=args[0], prop=args[1], value=args[2])
+
             elif '[' in line and ']' in line:
                 args = re.match(re.compile(
                     '^MERGE \(([0-9a-zA-Z]+)\)([-<]{1,2})\[:([A-Z,_]+)\]([->]{1,2})\(([0-9a-zA-Z]+)\)$'
                 ), line).groups()
                 if len(args) != 5:
-                    print("Nasol")
-                    exit(1)
+                    print(f"Unable to parse cypher relationship MERGE statement: {line}\n"
+                          f"The resulting table will not contain the effect of this statement, "
+                          f"so you should probably add it by hand in the table")
+                    continue
+                add_edge(graph=graph, node1=args[0], node2=args[4], verb=args[2], direction=args[3])
+
             elif 'MERGE' in line:
                 args = re.match(re.compile(
-                    '^MERGE \(([0-9a-zA-Z]+)((?::[a-zA-Z]+)+) {((?:[a-zA-Z]+:[0-9a-zA-Z]+(?:, )?)+)}\)$'
+                    '^MERGE \(([0-9a-zA-Z]+)((?::(?:[a-zA-Z]+)+)+)+(?: {((?:[a-zA-Z]+:[0-9a-zA-Z]+(?:, )?)+)})?\)$'
                 ), line).groups()
                 if len(args) != 3:
-                    print("Nasol")
-                    exit(1)
+                    print(f"Unable to parse cypher node MERGE statement: {line}"
+                          f"The resulting table will not contain the effect of this statement, "
+                          f"so you should probably add it by hand in the table")
+                    continue
+                merge_node(graph=graph, node_name=args[0], unparsed_labels=args[1], unparsed_properties=args[2])
+
             else:
-                print("Nasol rau")
-                exit(1)
+                print(f"Unable to parse unknown cypher statement: {line}"
+                      f"The resulting table will not contain the effect of this statement, "
+                      f"so you should probably add it by hand in the table")
+                continue
+            print(args)
 
             print(f"Graph: {graph}")
 
-            #"MERGE (p2:Product {docId:2, available:true, sensible:false, ignored:false})"
-            #"SET c1.lineName = 'Soft Lines' "
-            #"MERGE (p1)-[:IN_CATEGORY]->(c1)"
-            #"MERGE (p1)<-[:IS_MAIN_VENDOR]-(v1) "
-
     return Graph([], [])
 
-def merge_node(graph, )
+
+def merge_node(graph, node_name, unparsed_labels, unparsed_properties):
+    labels = unparsed_labels.split(":")[1:] if unparsed_labels is not None else []
+
+    properties = {
+        prop_name: prop_value for prop_name, prop_value in
+        [property.split(":") for property in unparsed_properties.split(", ")]
+    } if unparsed_properties is not None else {}
+
+    node = Node(name=node_name, labels=labels, edges=[], properties=properties)
+    graph.add_node(node)
+
+
+def add_edge(graph, node1, node2, verb, direction):
+    if direction == "->":
+        from_node = node1
+        to_node = node2
+    else:
+        from_node = node2
+        to_node = node1
+
+    edge = Edge(name=verb, from_node=from_node, to_node=to_node)
+    graph.add_edge(edge)
+
+
+def set_property(graph, node_name, prop, value):
+    node = [n for n in graph.nodes if n.name == node_name][0]
+    node.add_property(prop, value)
+
+
+if __name__ == '__main__':
+    cypher_to_graph("test")
--- graph.py
+++ graph.py
@@ -15,12 +15,20 @@ class Graph:
             if (e.from_node == n1 and e.to_node == n2) or (e.to_node == n1 and e.from_node == n2)
         ]
 
+    def __str__(self):
+        return f"Graph with {len(self.nodes)} nodes and {len(self.edges)} edges:\n" \
+               f"N: {self.nodes.__str__()}\n" \
+               f"E: {self.edges.__str__()}\n"
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Node:
-    def __init__(self):
-        self.labels = []
-        self.edges = []
-        self.properties = {}
+    def __init__(self, name, labels, edges, properties):
+        self.name = name
+        self.labels = labels
+        self.properties = properties
 
     def add_label(self, label):
         self.labels.append(label)
@@ -31,9 +39,21 @@ class Node:
     def has_label(self, label):
         return label in self.labels
 
+    def __str__(self):
+        return f"<{self.name}|{self.labels}|{self.properties}>"
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Edge:
     def __init__(self, name, from_node, to_node):
         self.name = name
         self.from_node = from_node
         self.to_node = to_node
+
+    def __str__(self):
+        return f"[{self.from_node}-{self.name}->{self.to_node}]"
+
+    def __repr__(self):
+        return self.__str__()
